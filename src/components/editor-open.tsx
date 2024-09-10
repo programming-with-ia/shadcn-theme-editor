8252a1f4286714a762add219ca632737960e9351
immi <iafullprogrammer@gmail.com>
2024-09-10 08:41:29 +0500

    v1.3.3

--- src/components/editor-open.tsx
+++ src/components/editor-open.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useState } from "react";
 import { Sidebar } from "./sidebar";
-import { IoColorPalette } from "react-icons/io5";
 import clsx from "clsx";
+import ColorPalette from "./icons";
 
 function EditorOpenBtn() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,7 +12,7 @@ function EditorOpenBtn() {
         onClick={() => setIsOpen(!isOpen)}
         className={clsx("rounded-full p-3 fixed border shadow-md drop-shadow-sm bottom-2 right-2 z-[35]", isOpen?"bg-primary text-primary-foreground":"text-primary bg-primary-foreground opacity-50 hover:opacity-100")}
       >
-        <IoColorPalette size={24} />
+        <ColorPalette size={24} />
       </button>
       <Sidebar state={{isOpen, setIsOpen}} />
     </div>
