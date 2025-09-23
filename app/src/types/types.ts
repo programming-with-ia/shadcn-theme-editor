a0282c406a934785494f7c6e5f168032ec2e4411
immi <iafullprogrammer@gmail.com>
2025-09-23 17:40:26 +0500

    clean

--- app/src/types/types.ts
+++
@@ -1,5 +0,0 @@
-import { JSXElementConstructor } from "react";
-
-export type CompoProps<T extends keyof JSX.IntrinsicElements | JSXElementConstructor<any>> =T extends keyof JSX.IntrinsicElements
-? JSX.IntrinsicElements[T]
-: React.ComponentProps<T>;
